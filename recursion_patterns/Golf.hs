92c7426761085338d7838186e072a645059fdde2
Vaibhav Pujari <vaibhav276@yahoo.co.in>
2016-05-31 12:01:30 +0530

    histogram function

--- recursion_patterns/Golf.hs
+++ recursion_patterns/Golf.hs
@@ -1,5 +1,8 @@
 module Golf where
 
+import Data.List
+
+-------------------Task 1------------------
 -- | @skips xs@ prepares a new list of lists, where each list is prepared by
 --   taking every nth element from the list @xs@, where n ranges from 1 to 
 --   length(xs)
@@ -12,6 +15,7 @@ getEvery n xs
    | n > length xs = []
    | otherwise     = head (drop (n - 1) xs) : ( getEvery n ( drop n xs) )
 
+-------------------Task 2------------------
 -- | @localMaxima xs@ returns a list of local maximas in a list
 --   A local maxima is an element which is larger than its adjescent elements
 localMaxima :: [Integer] -> [Integer]
@@ -20,3 +24,19 @@ localMaxima xs
    | otherwise     = 
    (if b > a && b > head c then [b] else []) ++ localMaxima (tail xs)
    where (a:b:c) = take 3 xs
+
+-------------------Task 3------------------
+-- | @histogram xs@ returns a putStr ready string containing the histogram of
+--   given data (only numbers 0 to 9 are processed, others are ignored)
+histogram :: [Integer] -> String
+histogram xs = plot xs ++ "==========\n0123456789\n"
+
+-- | @plot xs@ helps histogram function - prepares the plot part of the chart
+plot :: [Integer] -> String
+plot [] = ""
+plot xs = plot ( xs \\ nub xs ) ++ plotLine (nub xs) ++ "\n" 
+
+-- | @plotLine xs@ helps plot function - prepares the plot of a single line 
+--   Input should contain distinct elements to work as expected
+plotLine :: [Integer] -> String
+plotLine xs = [(\x xs -> if (elem x xs) then '*' else ' ') x xs | x <- [0..9]]
